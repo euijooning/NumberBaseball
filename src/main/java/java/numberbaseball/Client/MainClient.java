e2a9cc6468e4ddaf529e227a5a20f85d4ceae3a7
EUIJOON <euijoonism@gmail.com>
2023-06-07 20:16:09 +0900

    [feat] #13.main_client 부분 초안

---
+++ src/main/java/java/numberbaseball/Client/MainClient.java
@@ -0,0 +1,19 @@
+package java.numberbaseball.Client;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class MainClient {  //main client
+
+  public static void main(String[] args) {
+    try {
+      Socket cSocket = new Socket("localHost", 8102);
+      ClientReceiveThread rThread = new ClientReceiveThread();
+      rThread.setSocket(cSocket);
+      Thread t = new Thread(rThread);
+      t.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
