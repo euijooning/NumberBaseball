665c74626de91e2c1e5e2db5c8412e6bfefb6e31
EUIJOON <euijoonism@gmail.com>
2023-06-14 14:25:19 +0900

    [feat] CountResult 클래스 생성

---
+++ src/main/java/game/numberbaseball/Server/CountResult.java
@@ -0,0 +1,18 @@
+package game.numberbaseball.Server;
+
+import lombok.Builder;
+import lombok.Data; // 롬복 활용
+import lombok.EqualsAndHashCode;
+
+@Data
+@Builder
+@EqualsAndHashCode
+public class CountResult {
+
+  int strike = 0;
+  int ball = 0;
+  int out = 0;
+
+  public CountResult() {
+  }
+}
