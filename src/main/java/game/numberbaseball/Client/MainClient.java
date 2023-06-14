3c406fd9e478672ffed44667738b36dbed904a75
EUIJOON <euijoonism@gmail.com>
2023-06-14 11:14:17 +0900

    [refactor]ClientReceiveThread 역할 분리 -InputListener 분리 후 추가 수정

--- src/main/java/game/numberbaseball/Client/MainClient.java
+++ src/main/java/game/numberbaseball/Client/MainClient.java
@@ -11,9 +11,11 @@ public class MainClient {  //main client
       Socket cSocket = new Socket("localHost", 8102);
 
       ViewController viewController = new ViewController();
+
       ClientReceiveThread rThread = new ClientReceiveThread(viewController);
       // 이렇게 하면 viewController와 MainClient에서의 viewController가 똑같은 객체를 가리킴
-      viewController.init(rThread); // 조립하게 만들기
+
+      viewController.init(cSocket); // 조립하게 만들기
 
       rThread.setSocket(cSocket);
       Thread cThread = new Thread(rThread);
